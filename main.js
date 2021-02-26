10b47007c690a7eda6f5738bee991aa08d238857
Lorenzo M. Alù <lorenzo.alu@gmail.com>
2021-02-26 17:24:43 +0100

    risolti bug creata pagina inserisci annuncio

--- main.js
+++ main.js
@@ -6,17 +6,11 @@ const signbtn=document.querySelector("#signbtn")
 
 const signbtn1=document.querySelector("#signbtn1")
 
-
-
 logo.src="./media/logo.png"
-
 
 window.addEventListener("scroll", ()=> {
 
 let scrolled = window.pageYOffset
-
-
-
 
 if(scrolled>130) {
     navbar.classList.remove("bg-transparent")
@@ -25,12 +19,9 @@ if(scrolled>130) {
     signbtn.classList.remove("btn-outline-dark")
 
     signbtn1.classList.add("btn-outline-light")
-    signbtn1.classList.remove("btn-outline-dark")
-
-
+    signbtn1.classList.remove("btn-outline-dark","btn-accedi")
 
     logo.src="./media/logo-w.png"
-
 
 } else {
     navbar.classList.remove("navbar-dark","bg-secondary-color")
@@ -39,8 +30,7 @@ if(scrolled>130) {
     signbtn.classList.add("btn-outline-dark")
 
     signbtn1.classList.remove("btn-outline-light")
-    signbtn1.classList.add("btn-outline-dark")
-
+    signbtn1.classList.add("btn-outline-dark","btn-accedi")
 
     logo.src="./media/logo.png"
 
@@ -48,3 +38,45 @@ if(scrolled>130) {
 }
 
 })
+
+
+
+
+//effetto diamico per header principale 
+const skewedOne = document.querySelector('.skewedOne')
+
+const skewedTwo = document.querySelector('.skewedTwo')
+
+
+if(skewedOne,skewedTwo){
+window.addEventListener('scroll', () => {
+    const value1 = -15 + window.scrollY / 30
+    const value2 = 15 + window.scrollY / -30
+
+    skewedOne.style.transform = "sKewY(" + value1 + "deg)";
+    skewedTwo.style.transform = "sKewY(" + value2 + "deg)";
+
+})
+
+}
+
+
+//effetto dinamico per header small
+
+const skewedOneS = document.querySelector('.skewedOneS')
+
+        const skewedTwoS = document.querySelector('.skewedTwoS')
+
+
+        if(skewedOneS,skewedTwoS){
+
+        window.addEventListener('scroll', () => {
+            const value1 = -17 + window.scrollY / 15
+            const value2 = 17 + window.scrollY / -15
+
+            skewedOneS.style.transform = "sKewY(" + value1 + "deg)";
+            skewedTwoS.style.transform = "sKewY(" + value2 + "deg)";
+
+        })
+
+        }   
